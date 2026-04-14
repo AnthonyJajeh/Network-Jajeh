dbe6ef4a3956edc117fd6092100abc75afeed1ef
AnthonyJajeh <75507856+AnthonyJajeh@users.noreply.github.com>
2026-04-14 17:01:17 -0600

    Add files via upload

--- pde testing/pde_rhs.m
+++ pde testing/pde_rhs.m
@@ -1,203 +1,133 @@
 function dydt = pde_rhs(~,y,param)
-N_B= param.N_B;
+N_B = param.N_B;
 N_E = param.N_E;
 N_x = param.N_x;
-Delta_r_B = param.Delta_r_B;
-Delta_r_E = param.Delta_r_E;
-Delta_x   = param.Delta_x;
-
-r_B = param.r_B;     % r_{B,j}
-r_E = param.r_E;     % r_{E,j}
-u   = param.u;       % u(r_{B,j})
+n_pipes = param.n_pipes;
 
 D_B = param.D_B;
 D_E = param.D_E;
 lambda = param.lambda;
 
-%Number of stored unknowns per pipe
+dx = param.Delta_x;
+
+dr_B = param.Delta_r_B;
+dr_E = param.Delta_r_E;
+
+r_B_all = param.r_B;
+r_E_all = param.r_E;
+u_all   = param.u;
+
 n_B = (N_B-1)*(N_x-2);
 n_E = (N_E-1)*(N_x-2);
 
-% Block 1: B^1_{j,k}, j=1,...,N_B-1, k=2,...,N_x-1
-% Block 2: E^1_{j,k}, j=2,...,N_E,   k=2,...,N_x-1
-% Block 3: B^2_{j,k}, j=1,...,N_B-1, k=2,...,N_x-1
-% Block 4: E^2_{j,k}, j=2,...,N_E,   k=2,...,N_x-1
+dydt_blocks = cell(2*n_pipes,1);
 
-I_B1 = 1:n_B;
-I_E1 = n_B + (1:n_E);
-I_B2 = n_B + n_E + (1:n_B);
-I_E2 = n_B + n_E + n_B + (1:n_E);
+offset=0;
 
-% Unpack y into matrices of stored unknowns
+for p =1:n_pipes
+    drB = dr_B(p);
+    drE = dr_E(p);
 
-B1_stored = reshape(y(I_B1), N_B-1, N_x-2);
-E1_stored = reshape(y(I_E1), N_E-1, N_x-2);
-B2_stored = reshape(y(I_B2), N_B-1, N_x-2);
-E2_stored = reshape(y(I_E2), N_E-1, N_x-2);
+    rB = r_B_all(:,p);
+    rE = r_E_all(:,p);
+    u  = u_all(:,p);
+      % Unpack this pipe
+    B_stored = reshape(y(offset + (1:n_B)), N_B-1, N_x-2);
+    offset = offset + n_B;
 
-B1 = zeros(N_B,N_x);
-B2 = zeros(N_B,N_x);
-E1 = zeros(N_E,N_x);
-E2 = zeros(N_E,N_x);
+    E_stored = reshape(y(offset + (1:n_E)), N_E-1, N_x-2);
+    offset = offset + n_E;
 
-B1(1:N_B-1,2:N_x-1) = B1_stored;
-B2(1:N_B-1,2:N_x-1) = B2_stored;
-E1(2:N_E,2:N_x-1) = E1_stored;
-E2(2:N_E,2:N_x-1) = E2_stored;
+    % Reconstruct full arrays
+    B = zeros(N_B,N_x);
+    E = zeros(N_E,N_x);
 
-%BC for brine
-B1(:,1) = param.C_in;
-B1(:,N_x)=param.C_con;
-
-B2(:,1) = param.C_con;
-B2(:,N_x) = param.C_out;
-
-%BC for EPS
-E1(:,1)= E1(:,2);
-E1(:,N_x) = E1(:,N_x-1);
-
-E2(:,1)= E2(:,2);
-E2(:,N_x) = E2(:,N_x-1);
-
-%at r=a matching 
-% Continuity, E_{1,k} = B_{N_B,k}
-% Derivative matching  = (D_BDelta_r_E B^i_{N_B-1,k} + D_EDelta_r_B E^i_{2,k})
-%   /(D_BDelta_r_B + D_EDelta_r_E)
-
-for k = 2:N_x-1
-    B1(N_B,k) = (D_B*Delta_r_E*B1(N_B-1,k)+D_E*Delta_r_B*E1(2,k))/(D_B*Delta_r_B-D_E*Delta_r_E);
-    E1(1,k)=B1(N_B,k);
-    B2(N_B,k) = (D_B*Delta_r_E*B2(N_B-1,k)+D_E*Delta_r_B*E2(2,k))/(D_B*Delta_r_B-D_E*Delta_r_E);
-    E2(1,k) = B2(N_B,k);
-end
-%Interface values at axial boundaries
-E1(1,1)=B1(N_B,1);
-E1(1,N_x)=B1(N_B,N_x);
-E2(1,1)=B2(N_B,1);
-E2(1,N_x)=B2(N_B,N_x);
-
-%time derivative allocation
-dB1_dt = zeros(N_B-1, N_x-2);
-dE1_dt = zeros(N_E-1, N_x-2);
-dB2_dt = zeros(N_B-1, N_x-2);
-dE2_dt = zeros(N_E-1, N_x-2);
-
-%pipe 1 brine 
-for k = 2:N_x-1
-    k_interior = k - 1;
-
-    % ----------------------------
-    % j = 1 : centerline r = 0
-    % ----------------------------
-    j = 1;
-
-    radial_term = 4*(B1(2,k) - B1(1,k))/Delta_r_B^2;
-
-    axial_diffusion = (B1(j,k+1) - 2*B1(j,k) + B1(j,k-1))/Delta_x^2;
-
-    axial_advection = u(j)*(B1(j,k) - B1(j,k-1))/Delta_x;
-
-    dB1_dt(j,k_interior) = D_B*(radial_term + axial_diffusion) - axial_advection;
-
-    % ----------------------------
-    % j = 2,...,N_B-1 : interior brine nodes
-    % ----------------------------
-    for j = 2:N_B-1
-        radial_term = (B1(j+1,k) - 2*B1(j,k) + B1(j-1,k))/Delta_r_B^2 ...
-                    + (1/r_B(j))*(B1(j+1,k) - B1(j-1,k))/(2*Delta_r_B);
-
-        axial_diffusion = (B1(j,k+1) - 2*B1(j,k) + B1(j,k-1))/Delta_x^2;
-
-        axial_advection = u(j)*(B1(j,k) - B1(j,k-1))/Delta_x;
-
-        dB1_dt(j,k_interior) = D_B*(radial_term + axial_diffusion)- axial_advection;
-                               
-    end
-end
-
-%pipe 1 EPS
-for k = 2:N_x-1
-    k_interior = k - 1;
-
-    % ----------------------------
-    % j = 2,...,N_E-1 : interior EPS nodes
-    % ----------------------------
-    for j = 2:N_E-1
-        radial_term = (E1(j+1,k) - 2*E1(j,k) + E1(j-1,k))/Delta_r_E^2 ...
-                    + (1/r_E(j))*(E1(j+1,k) - E1(j-1,k))/(2*Delta_r_E);
-
-        axial_diffusion = (E1(j,k+1) - 2*E1(j,k) + E1(j,k-1))/Delta_x^2;
-
-        dE1_dt(j-1,k_interior) = D_E*(radial_term + axial_diffusion) ...
-                                 - lambda*E1(j,k);
+    B(1:N_B-1,2:N_x-1) = B_stored;
+    E(2:N_E,2:N_x-1)   = E_stored;
+  % Axial BCs for brine
+    if p == 1
+        B(:,1) = param.C_in;
+    else
+        B(:,1) = prev_pipe_outlet;
     end
 
-    % ----------------------------
-    % j = N_E : outer wall r = R
-    % Use no-flux condition dE/dr = 0
-    % ----------------------------
-    j = N_E;
+    % Top outflow for brine interior rows only
+    B(1:N_B-1,N_x) = B(1:N_B-1,N_x-1);
 
-    radial_term = 2*(E1(N_E-1,k) - E1(N_E,k))/Delta_r_E^2;
+    % Axial BCs for EPS: zero x-derivative
+    E(:,1)   = E(:,2);
+    E(:,N_x) = E(:,N_x-1);
 
-    axial_diffusion = (E1(j,k+1) - 2*E1(j,k) + E1(j,k-1))/Delta_x^2;
-
-    dE1_dt(N_E-1,k_interior) = D_E*(radial_term + axial_diffusion) ...
-                               - lambda*E1(j,k);
-end
-
-%pipe 2 brine
-for k = 2:N_x-1
-    k_interior = k - 1;
-
-    % j = 1
-    j = 1;
-
-    radial_term = 4*(B2(2,k) - B2(1,k))/Delta_r_B^2;
-
-    axial_diffusion = (B2(j,k+1) - 2*B2(j,k) + B2(j,k-1))/Delta_x^2;
-
-    axial_advection = u(j)*(B2(j,k) - B2(j,k-1))/Delta_x;
-
-    dB2_dt(j,k_interior) = D_B*(radial_term + axial_diffusion) ...
-                           - axial_advection;
-
-    % j = 2,...,N_B-1
-    for j = 2:N_B-1
-        radial_term = (B2(j+1,k) - 2*B2(j,k) + B2(j-1,k))/Delta_r_B^2 ...
-                    + (1/r_B(j))*(B2(j+1,k) - B2(j-1,k))/(2*Delta_r_B);
-
-        axial_diffusion = (B2(j,k+1) - 2*B2(j,k) + B2(j,k-1))/Delta_x^2;
-
-        axial_advection = u(j)*(B2(j,k) - B2(j,k-1))/Delta_x;
-
-        dB2_dt(j,k_interior) = D_B*(radial_term + axial_diffusion) ...
-                               - axial_advection;
-    end
-end
-
-%pipe 2 EPS
-for k = 2:N_x-1
-    k_interior = k - 1;
-
-    for j = 2:N_E-1
-        radial_term = (E2(j+1,k) - 2*E2(j,k) + E2(j-1,k))/Delta_r_E^2 ...
-                    + (1/r_E(j))*(E2(j+1,k) - E2(j-1,k))/(2*Delta_r_E);
-
-        axial_diffusion = (E2(j,k+1) - 2*E2(j,k) + E2(j,k-1))/Delta_x^2;
-
-        dE2_dt(j-1,k_interior) = D_E*(radial_term + axial_diffusion) ...
-                                 - lambda*E2(j,k);
+    % Interface reconstruction at r = a(p)
+    for k = 2:N_x-1
+        B(N_B,k) = (D_B*drE*B(N_B-1,k) + D_E*drB*E(2,k)) ...
+                 / (D_B*drE + D_E*drB);
+        E(1,k) = B(N_B,k);
     end
 
-    j = N_E;
+ % Boundary interface values
+    B(N_B,1)   = B(N_B,2);
+    B(N_B,N_x) = B(N_B,N_x-1);
 
-    radial_term = 2*(E2(N_E-1,k) - E2(N_E,k))/Delta_r_E^2;
+    E(1,1)   = B(N_B,1);
+    E(1,N_x) = B(N_B,N_x);
 
-    axial_diffusion = (E2(j,k+1) - 2*E2(j,k) + E2(j,k-1))/Delta_x^2;
+    % Save outlet of this pipe for next pipe
+    prev_pipe_outlet = B(:,N_x);
 
-    dE2_dt(N_E-1,k_interior) = D_E*(radial_term + axial_diffusion) ...
-                               - lambda*E2(j,k);
+    % Time derivatives
+    dB_dt = zeros(N_B-1, N_x-2);
+    dE_dt = zeros(N_E-1, N_x-2);
+
+ %Brine equation
+  for k = 2:N_x-1
+        kk = k - 1;
+
+        % centerline j = 1
+        radial_term = 4*(B(2,k) - B(1,k))/drB^2;
+        axial_diff  = (B(1,k+1) - 2*B(1,k) + B(1,k-1))/dx^2;
+        axial_adv   = u(1)*(B(1,k) - B(1,k-1))/dx;
+
+        dB_dt(1,kk) = D_B*(radial_term + axial_diff) - axial_adv;
+
+
+        % interior j = 2,...,N_B-1
+        for j = 2:N_B-1
+            radial_term = (B(j+1,k) - 2*B(j,k) + B(j-1,k))/drB^2 ...
+                        + (1/rB(j))*(B(j+1,k) - B(j-1,k))/(2*drB);
+
+            axial_diff = (B(j,k+1) - 2*B(j,k) + B(j,k-1))/dx^2;
+            axial_adv  = u(j)*(B(j,k) - B(j,k-1))/dx;
+
+            dB_dt(j,kk) = D_B*(radial_term + axial_diff) - axial_adv;
+        end
+  end
+ % -------------------------
+    % EPS equation
+    % -------------------------
+    for k = 2:N_x-1
+        kk = k - 1;
+
+        % interior j = 2,...,N_E-1
+        for j = 2:N_E-1
+            radial_term = (E(j+1,k) - 2*E(j,k) + E(j-1,k))/drE^2 ...
+                        + (1/rE(j))*(E(j+1,k) - E(j-1,k))/(2*drE);
+
+            axial_diff = (E(j,k+1) - 2*E(j,k) + E(j,k-1))/dx^2;
+
+            dE_dt(j-1,kk) = D_E*(radial_term + axial_diff) - lambda*E(j,k);
+        end
+
+        % outer wall j = N_E, no-flux
+        j = N_E;
+        radial_term = 2*(E(N_E-1,k) - E(N_E,k))/drE^2;
+        axial_diff  = (E(j,k+1) - 2*E(j,k) + E(j,k-1))/dx^2;
+
+        dE_dt(N_E-1,kk) = D_E*(radial_term + axial_diff) - lambda*E(j,k);
+    end
+    dydt_blocks{2*p-1} = dB_dt(:);
+    dydt_blocks{2*p}   = dE_dt(:);
 end
-dydt = [dB1_dt(:); dE1_dt(:); dB2_dt(:); dE2_dt(:)];
+
+dydt = vertcat(dydt_blocks{:});
 end
--- pde testing/pde_sim.m
+++ pde testing/pde_sim.m
@@ -4,154 +4,260 @@ clear; clc; close all
 D_B = 1;
 D_E = .02;
 lambda = 0.1;
-
-% Pipe 1 radii
-a_1 = 1.0;
-R_1 = 2.0;
-
-% Pipe 2 radii
-a_2 = 0.7;
-R_2 = 1.6;
 L=5;
 
+N_B = 50;     % number of brine radial nodes
+N_E = 50;     % number of EPS radial nodes
+N_x = 50;     % number of axial nodes
+
+
+% Pipe radii: one entry per pipe
+a = [1.0, 1.3];     % brine/interface radii
+R = [2.0, 1.7];     % outer EPS radii
+n_pipes = numel(a);
+
+r_B = zeros(N_B, n_pipes);
+r_E = zeros(N_E, n_pipes);
+u   = zeros(N_B, n_pipes);
+
+Delta_r_B = zeros(1,n_pipes);
+Delta_r_E = zeros(1,n_pipes);
+Delta_x =L/(N_x-1);
+x_local   = linspace(0,L,N_x);
+for p = 1:n_pipes
+    Delta_r_B(p) = a(p)/(N_B-1);
+    Delta_r_E(p) = (R(p)-a(p))/(N_E-1);
+
+    r_B(:,p) = linspace(0,a(p),N_B)';
+    r_E(:,p) = linspace(a(p),R(p),N_E)';
+
+    % Example velocity profile in brine region
+    u(:,p) = 1 - (r_B(:,p)/a(p)).^2;
+end
+
+%Initial condition concentrations 
 C_in  = 1.0;
-C_con = 0.5;
-C_out = 0.0;
 
-N_B = 10;     % number of brine radial nodes
-N_E = 10;     % number of EPS radial nodes
-N_x = 20;     % number of axial nodes
-
-Delta_r_B1 = a_1/(N_B-1);
-Delta_r_E1 = (R_1-a_1)/(N_E-1);
-Delta_r_B2 = a_2/(N_B-1);
-Delta_r_E2 = (R_2-a_2)/(N_E-1);
-Delta_x   = L/(N_x-1);
-
-r_B1 = linspace(0,a_1,N_B)';     % r_{B,j}
-r_E1 = linspace(a_1,R_1,N_E)';     % r_{E,j}
-r_B2 = linspace(0,a_2,N_B)';     % r_{B,j}
-r_E1 = linspace(a_2,R_2,N_B)';     % r_{B,j}
-
-x   = linspace(0,L,N_x);      % x_k
-
-% Example velocity u(r) in the brine region
-% Velocity profiles
-u1 = 1 - (r_B1/a1).^2;
-u2 = 1 - (r_B2/a2).^2;
-
+params.a= a;
+params.R = R;
 params.D_B = D_B;
 params.D_E = D_E;
 params.lambda = lambda;
-params.a_1 = a_1;
-params.a_2= a_2;
-params.R_1=R_1;
-params.R_2 = R_2;
 params.L = L;
 
 params.C_in = C_in;
-params.C_con = C_con;
-params.C_out = C_out;
 
 params.N_B = N_B;
 params.N_E = N_E;
 params.N_x = N_x;
+params.n_pipes = n_pipes;
 
-params.Delta_r_B1 = Delta_r_B1;
-params.Delta_r_E1 = Delta_r_E1;
+params.Delta_r_B = Delta_r_B;
+params.Delta_r_E = Delta_r_E;
+params.Delta_x   = Delta_x;
 
-params.Delta_r_B2 = Delta_r_B2;
-params.Delta_r_E2 = Delta_r_E2;
-params.Delta_x = Delta_x;
+params.r_B = r_B;
+params.r_E = r_E;
+params.u   = u;
 
-params.r_B1 = r_B1;
-params.r_E1 = r_E1;
-
-params.r_B2 = r_B2;
-params.r_E2 = r_E2;
-params.u = u;
-
-% Initial conditions for stored unknowns
-% B^1_{j,k}, j=1,...,N_B-1, k=2,...,N_x-1
-B1_initial = zeros(N_B-1, N_x-2);
-
-% E^1_{j,k}, j=2,...,N_E, k=2,...,N_x-1
-E1_initial = zeros(N_E-1, N_x-2);
-
-% B^2_{j,k}
-B2_initial = zeros(N_B-1, N_x-2);
-
-% E^2_{j,k}
-E2_initial = zeros(N_E-1, N_x-2);
-
-% Stack into one long vector
-y0 = [B1_initial(:); E1_initial(:); B2_initial(:); E2_initial(:)];
-
-tspan = [0 10];
-opts = odeset('RelTol',1e-6,'AbsTol',1e-8);
-
-[t,y] = ode23s(@(t,y) pde_rhs(t,y,params), tspan, y0, opts);
-
-m = length(t);
-y_m = y(m,:)';
-
+% Initial conditions
 n_B = (N_B-1)*(N_x-2);
 n_E = (N_E-1)*(N_x-2);
 
-B1_stored = reshape(y_m(1:n_B), N_B-1, N_x-2);
-E1_stored = reshape(y_m(n_B+(1:n_E)), N_E-1, N_x-2);
-B2_stored = reshape(y_m(n_B+n_E+(1:n_B)), N_B-1, N_x-2);
-E2_stored = reshape(y_m(n_B+n_E+n_B+(1:n_E)), N_E-1, N_x-2);
 
-B1_interface = (D_B*Delta_r_E*B1_stored(end,:) + D_E*Delta_r_B*E1_stored(1,:)) ...
-               /(D_B*Delta_r_B + D_E*Delta_r_E);
+% Stack into one long vector
+y0 = zeros(n_pipes*(n_B+n_E),1);
 
-E1_interface = B1_interface;
+tspan = [0 10];
+opts = odeset('RelTol',1e-5,'AbsTol',1e-7);
+tic
+[t,y] = ode15s(@(t,y) pde_rhs(t,y,params), tspan, y0, opts);
+toc
+% Final time solution
+y_m = y(end,:)';
 
-B2_interface = (D_B*Delta_r_E*B2_stored(end,:) + D_E*Delta_r_B*E2_stored(1,:)) ...
-               /(D_B*Delta_r_B + D_E*Delta_r_E);
+for p = 1:n_pipes
 
-E2_interface = B2_interface;
+    offset = (p-1)*(n_B+n_E);
 
-x_int = x(2:N_x-1);
-B1_plot = [B1_stored; B1_interface];
-B2_plot = [B2_stored; B2_interface];
+    B_stored = reshape(y_m(offset + (1:n_B)), N_B-1, N_x-2);
+    E_stored = reshape(y_m(offset + n_B + (1:n_E)), N_E-1, N_x-2);
 
-rB_plot = [r_B(1:N_B-1); a];
+    drB = Delta_r_B(p);
+    drE = Delta_r_E(p);
 
-E1_plot = [E1_interface; E1_stored];
-E2_plot = [E2_interface; E2_stored];
+    % Interface nutrient value at r = a(p)
+    B_interface = (D_B*drE*B_stored(end,:) + D_E*drB*E_stored(1,:)) ...
+                / (D_B*drE + D_E*drB);
 
-rE_plot = [a; r_E(2:N_E)];
+    % Right half of the pipe: brine + interface + EPS
+    N_right = [B_stored; B_interface; E_stored(2:end,:)];
+    r_right = [r_B(1:N_B-1,p); a(p); r_E(2:end,p)];
 
+    % Mirror to full pipe
+    N_full = [flipud(N_right(2:end,:)); N_right];
+    r_full = [-flipud(r_right(2:end)); r_right];
+
+    % Axial location of this pipe in the global domain
+    x_plot = x_local(2:N_x-1) + (p-1)*L;
+
+    figure;
+   imagesc(r_full, x_plot, N_full');
+    set(gca,'YDir','normal');
+    xlim([-R(p) R(p)]);
+    ylim([x_plot(1) x_plot(end)]);
+    xlabel('r');
+    ylabel('x');
+    title(sprintf('Pipe %d nutrient everywhere', p));
+    colorbar
+    hold on
+    plot([ a(p)  a(p)], [x_plot(1) x_plot(end)], 'w--', 'LineWidth', 1.5);
+    plot([-a(p) -a(p)], [x_plot(1) x_plot(end)], 'w--', 'LineWidth', 1.5);
+    plot([ R(p)  R(p)], [x_plot(1) x_plot(end)], 'k-', 'LineWidth', 1.5);
+    plot([-R(p) -R(p)], [x_plot(1) x_plot(end)], 'k-', 'LineWidth', 1.5);
+    hold off
+end
+
+% -------- Combined stacked plot for all pipes --------
 figure;
-imagesc(rB_plot,x_int, B1_plot);
-set(gca,'YDir','normal');
-xlabel('r_B');
-ylabel('x');
-title('Pipe 1 brine including interface');
-colorbar;
+hold on
 
-figure;
-imagesc(rB_plot,x_int, B2_plot);
-set(gca,'YDir','normal');
-xlabel('r_B');
-ylabel('x');
-title('Pipe 2 brine including interface');
-colorbar;
+% global radial extent across all pipes
+Rmax = max(R);
 
-figure;
-imagesc(rE_plot,x_int, E1_plot);
-set(gca,'YDir','normal');
-xlabel('r_E');
-ylabel('x');
-title('Pipe 1 EPS including interface');
-colorbar;
+% optional: common color scale from final-time data
+cmin = inf;
+cmax = -inf;
 
-figure;
-imagesc(rE_plot,x_int, E2_plot);
+% first pass: get common color limits
+for p = 1:n_pipes
+    offset = (p-1)*(n_B+n_E);
+
+    B_stored = reshape(y_m(offset + (1:n_B)), N_B-1, N_x-2);
+    E_stored = reshape(y_m(offset + n_B + (1:n_E)), N_E-1, N_x-2);
+
+    drB = Delta_r_B(p);
+    drE = Delta_r_E(p);
+
+    B_interface = (D_B*drE*B_stored(end,:) + D_E*drB*E_stored(1,:)) ...
+                / (D_B*drE + D_E*drB);
+
+    N_right = [B_stored; B_interface; E_stored(2:end,:)];
+    N_full  = [flipud(N_right(2:end,:)); N_right];
+
+    cmin = min(cmin, min(N_full(:)));
+    cmax = max(cmax, max(N_full(:)));
+end
+
+% second pass: draw each pipe on its own x-interval
+for p = 1:n_pipes
+    offset = (p-1)*(n_B+n_E);
+
+    B_stored = reshape(y_m(offset + (1:n_B)), N_B-1, N_x-2);
+    E_stored = reshape(y_m(offset + n_B + (1:n_E)), N_E-1, N_x-2);
+
+    drB = Delta_r_B(p);
+    drE = Delta_r_E(p);
+
+    B_interface = (D_B*drE*B_stored(end,:) + D_E*drB*E_stored(1,:)) ...
+                / (D_B*drE + D_E*drB);
+
+    % right half: brine core + interface + EPS layer
+    N_right = [B_stored; B_interface; E_stored(2:end,:)];
+    r_right = [r_B(1:N_B-1,p); a(p); r_E(2:end,p)];
+
+    % mirror to full pipe
+    N_full = [flipud(N_right(2:end,:)); N_right];
+    r_full = [-flipud(r_right(2:end)); r_right];
+
+    % global x-location for this pipe
+    x_plot = x_local(2:N_x-1) + (p-1)*L;
+
+    % draw the data
+    imagesc(r_full, x_plot, N_full');
+    
+    % draw interface and wall lines
+    plot([ a(p)  a(p)], [x_plot(1) x_plot(end)], 'w--', 'LineWidth', 1.5);
+    plot([-a(p) -a(p)], [x_plot(1) x_plot(end)], 'w--', 'LineWidth', 1.5);
+    plot([ R(p)  R(p)], [x_plot(1) x_plot(end)], 'k-', 'LineWidth', 1.5);
+    plot([-R(p) -R(p)], [x_plot(1) x_plot(end)], 'k-', 'LineWidth', 1.5);
+end
+
 set(gca,'YDir','normal');
-xlabel('r_E');
-ylabel('x');
-title('Pipe 2 EPS including interface');
-colorbar;
+axis tight
+xlim([-Rmax Rmax]);
+ylim([x_local(2) x_local(end-1) + (n_pipes-1)*L]);
+caxis([cmin cmax]);
+
+xlabel('r');
+ylabel('global x');
+title('Nutrient everywhere across stacked pipes');
+colorbar
+hold off
+
+% -------- Time animation for nutrient everywhere --------
+for p = 1:n_pipes   % choose which pipe to animate
+figure;
+
+% optional fixed color scale
+cmin = inf;
+cmax = -inf;
+for n = 1:length(t)
+    y_n = y(n,:)';
+
+    offset = (p-1)*(n_B+n_E);
+    B_stored = reshape(y_n(offset + (1:n_B)), N_B-1, N_x-2);
+    E_stored = reshape(y_n(offset + n_B + (1:n_E)), N_E-1, N_x-2);
+
+    drB = Delta_r_B(p);
+    drE = Delta_r_E(p);
+
+    B_interface = (D_B*drE*B_stored(end,:) + D_E*drB*E_stored(1,:)) ...
+                / (D_B*drE + D_E*drB);
+
+    N_right = [B_stored; B_interface; E_stored(2:end,:)];
+    N_full = [flipud(N_right(2:end,:)); N_right];
+
+    cmin = min(cmin, min(N_full(:)));
+    cmax = max(cmax, max(N_full(:)));
+end
+
+for n = 1:length(t)
+    y_n = y(n,:)';
+
+    offset = (p-1)*(n_B+n_E);
+
+    B_stored = reshape(y_n(offset + (1:n_B)), N_B-1, N_x-2);
+    E_stored = reshape(y_n(offset + n_B + (1:n_E)), N_E-1, N_x-2);
+
+    drB = Delta_r_B(p);
+    drE = Delta_r_E(p);
+
+    B_interface = (D_B*drE*B_stored(end,:) + D_E*drB*E_stored(1,:)) ...
+                / (D_B*drE + D_E*drB);
+
+    N_right = [B_stored; B_interface; E_stored(2:end,:)];
+    r_right = [r_B(1:N_B-1,p); a(p); r_E(2:end,p)];
+
+    N_full = [flipud(N_right(2:end,:)); N_right];
+    r_full = [-flipud(r_right(2:end)); r_right];
+
+    x_plot = x_local(2:N_x-1) + (p-1)*L;
+
+    clf
+    imagesc(r_full, x_plot, N_full');
+    set(gca,'YDir','normal');
+    caxis([cmin cmax]);
+    xlabel('r');
+    ylabel('global x');
+    title(sprintf('Pipe %d nutrient everywhere, t = %.2f', p, t(n)));
+    colorbar
+    hold on
+    plot([ a(p)  a(p)], [x_plot(1) x_plot(end)], 'w--', 'LineWidth', 1.5);
+    plot([-a(p) -a(p)], [x_plot(1) x_plot(end)], 'w--', 'LineWidth', 1.5);
+    plot([ R(p)  R(p)], [x_plot(1) x_plot(end)], 'k-', 'LineWidth', 1.5);
+    plot([-R(p) -R(p)], [x_plot(1) x_plot(end)], 'k-', 'LineWidth', 1.5);
+    hold off
+    drawnow
+end
+end
